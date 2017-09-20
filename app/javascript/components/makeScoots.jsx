df4332dc8bee7ae6f7dc9cf7e765d8df3757ef78
Michael Groeneman <michael@groeneman.com>
2017-09-20 14:06:56 -0700

    Changeable label

--- app/javascript/components/makeScoots.jsx
+++ app/javascript/components/makeScoots.jsx
@@ -4,13 +4,14 @@ import Scooter from './scooter';
 import { addScoot } from './actions';
 
 const MakeScoots = ({ scoots, addScoot }) => {
+  const label = scoots.length > 0 ? 'Add another!' : 'Add a scoot!';
   return (
     <div className="wrapper">
       <div className="list-of-scoots">
         { scoots.map(scoot => <Scooter key={ scoot.name } { ...scoot } />) }
       </div>
 
-      <button onClick={ addScoot }>Add another!</button>
+      <button onClick={ addScoot }>{ label }</button>
     </div>
   );
 }
